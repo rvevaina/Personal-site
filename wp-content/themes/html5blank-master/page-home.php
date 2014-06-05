6fc496fb4aebcc8f03b1924fb6fb20039737bad7
rayhan_v@me.com <rayhan_v@me.com>
2014-06-05 17:41:06 -0400

    portfolio

--- wp-content/themes/html5blank-master/page-home.php
+++ wp-content/themes/html5blank-master/page-home.php
@@ -26,12 +26,12 @@ get_header() ?>
  
  <?php $allpages = new WP_Query(
  	array (
- 		"page_id" => 50
- 		// "posts_per_page" => -1,
- 		// "post_type" => 'page',
- 		// "order" => 'ASC',
- 		// "orderby" => "menu_order", 
- 		// "post__not_in" =>array(17,2),
+ 		// "page_id" => 50
+ 		"posts_per_page" => -1,
+ 		"post_type" => 'page',
+ 		"order" => 'ASC',
+ 		"orderby" => "menu_order", 
+ 		"post__not_in" =>array(17,2,7),
  		 )
  	); ?>
 
@@ -59,6 +59,21 @@ get_header() ?>
 		</div>
 		<!-- /article -->
 
+
+   <?php endwhile; ?>
+
+	<?php else: ?>
+
+		<!-- article -->
+		<article>
+
+			<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+		</article>
+		<!-- /article -->
+
+	<?php endif; ?>
+
 	<div id="skills" class="clearfix"> 
    		<h2><span>SKILLS</span></h2>
    	  	<div class="skillscontainer">	
@@ -76,30 +91,60 @@ get_header() ?>
 
 			<div class="design">
 				<img src="<?php echo get_template_directory_uri(); ?>/img/responsive1.png" alt="">
-	           
-				
+	          
 				<ul>
-				<h3>Responsive Design</h3>
-				<li>Cross browser compatibility</li>
-				<li>WordPress CMS Design</li>
-				<li>User Experience</li>
+					<h3>Responsive Design</h3>
+					<li>Cross browser compatibility</li>
+					<li>WordPress CMS Design</li>
+					<li>User Experience</li>
 				</ul>
 			</div>
 
 			<div class="writing">
 				<img src="<?php echo get_template_directory_uri(); ?>/img/writing.png" alt="">
-						
-				<h3>Copywriting</h3>
-				
+								
 				<ul>
-				<li>Website copy</li>
-				<li>Proofreading and editing</li>
-				<li>Managing content</li>
+					<h3>Copywriting</h3>
+					<li>Proofreading and editing</li>
+					<li>Managing content</li>
+					<li>Website copy</li>
 				</ul>
 			</div>
 
    	  </div>
    </div>
+
+	</section>
+	<!-- /section -->
+	
+
+<?php $portfolio = new WP_Query(
+ 	array (
+ 		"post_type" => 'portfolio',
+ 		"order" => 'ascending',
+ 		"orderby" => "menu_order", 
+ 		 )
+ 	); ?>
+
+   
+	<?php if ($portfolio->have_posts()): while ($portfolio->have_posts()) : $portfolio->the_post(); ?>
+
+		<!-- article -->
+				<h2><?php the_title(); ?></h2>
+
+
+			<?php the_content(); ?>
+
+			<br class="clear">
+			
+			<!-- styling the thumbnail image - ask instructor-->
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?>
+			<?php the_post_thumbnail('medium'); ?></a>
+
+
+		</div>
+		<!-- /article -->
+
 
 	<?php endwhile; ?>
 
